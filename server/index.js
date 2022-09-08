a836cec5afe039e0907c27b076156c8ebbd94f1c
Yaki <tzyakov@gmail.com>
2022-09-09 01:40:38 +0300

    15

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ const app = express();
 
 /* Un mark before deployment*/
 
-// // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // middleware
 app.use(express.json());
