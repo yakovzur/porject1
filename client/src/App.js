0058fe1aabf399737ae3d013d6572b097e9c430f
Yaki <tzyakov@gmail.com>
2022-09-08 21:26:51 +0300

    5

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
         <h1>Very cool</h1>
 
