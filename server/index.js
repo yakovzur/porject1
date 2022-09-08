772fcd0d4b87274ec554bc0721ce2d2d5ef191dd
Yaki <tzyakov@gmail.com>
2022-09-09 01:30:57 +0300

    12

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-//   });
+});
 
 // middleware
 app.use(express.json());
